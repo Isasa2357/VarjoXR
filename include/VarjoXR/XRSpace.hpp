c850b1b7ece133e89fc8afcd057dab95c9c50e6c
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-07 23:00:33 +0900

    Expose native D3D texture wrapping on XRSpace

--- include/VarjoXR/XRSpace.hpp
+++ include/VarjoXR/XRSpace.hpp
@@ -9,6 +9,13 @@
 #include <memory>
 #include <vector>
 
+#if defined(VARJOXR_ENABLE_D3D11)
+#include <d3d11.h>
+#endif
+#if defined(VARJOXR_ENABLE_D3D12)
+#include <d3d12.h>
+#endif
+
 namespace VarjoXR {
 
 class VarjoSession;
@@ -19,6 +26,9 @@ struct XRSpaceConfig {
     BackendType backend = BackendType::D3D11;
     int32_t swapChainTextureCount = 3;
     int64_t layerFlags = 0;
+    // 0 means "use VarjoXR default". Runtime backends currently resolve it to
+    // varjo_TextureFormat_R8G8B8A8_UNORM so that users do not accidentally pass
+    // varjo_TextureFormat_INVALID to swapchain creation.
     int64_t colorFormat = 0;
     bool enableDebug = true;
 };
@@ -38,6 +48,22 @@ public:
         uint32_t width,
         uint32_t height,
         uint32_t rowPitchBytes = 0);
+
+#if defined(VARJOXR_ENABLE_D3D11)
+    std::shared_ptr<XRTexture> createTextureFromD3D11Resource(
+        ID3D11Texture2D* texture,
+        DXGI_FORMAT srvFormat = DXGI_FORMAT_UNKNOWN);
+    std::shared_ptr<XRTexture> createTextureFromD3D11Srv(
+        ID3D11ShaderResourceView* srv,
+        uint32_t width,
+        uint32_t height);
+#endif
+
+#if defined(VARJOXR_ENABLE_D3D12)
+    std::shared_ptr<XRTexture> createTextureFromD3D12Resource(
+        ID3D12Resource* resource,
+        DXGI_FORMAT srvFormat = DXGI_FORMAT_UNKNOWN);
+#endif
 
     void update();
 
