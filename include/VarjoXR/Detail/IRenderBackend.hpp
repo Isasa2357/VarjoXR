b5d6b09f29ef7307faa6ea1d6413c5a6f7fe6a71
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-07 23:00:23 +0900

    Add native texture wrapping hooks to render backend

--- include/VarjoXR/Detail/IRenderBackend.hpp
+++ include/VarjoXR/Detail/IRenderBackend.hpp
@@ -8,6 +8,13 @@
 #include <memory>
 #include <vector>
 
+#if defined(VARJOXR_ENABLE_D3D11)
+#include <d3d11.h>
+#endif
+#if defined(VARJOXR_ENABLE_D3D12)
+#include <d3d12.h>
+#endif
+
 namespace VarjoXR {
 
 class VarjoSession;
@@ -25,6 +32,22 @@ public:
         uint32_t width,
         uint32_t height,
         uint32_t rowPitchBytes) = 0;
+
+#if defined(VARJOXR_ENABLE_D3D11)
+    virtual std::shared_ptr<XRTexture> createTextureFromD3D11Resource(
+        ID3D11Texture2D* texture,
+        DXGI_FORMAT srvFormat) = 0;
+    virtual std::shared_ptr<XRTexture> createTextureFromD3D11Srv(
+        ID3D11ShaderResourceView* srv,
+        uint32_t width,
+        uint32_t height) = 0;
+#endif
+
+#if defined(VARJOXR_ENABLE_D3D12)
+    virtual std::shared_ptr<XRTexture> createTextureFromD3D12Resource(
+        ID3D12Resource* resource,
+        DXGI_FORMAT srvFormat) = 0;
+#endif
 };
 
 } // namespace VarjoXR
