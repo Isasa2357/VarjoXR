337458c532fd971ceba16c6909713aede60bfa8f
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-07 23:06:00 +0900

    Make backend-specific texture wrappers optional virtual hooks

--- include/VarjoXR/Detail/IRenderBackend.hpp
+++ include/VarjoXR/Detail/IRenderBackend.hpp
@@ -6,6 +6,7 @@
 
 #include <cstdint>
 #include <memory>
+#include <stdexcept>
 #include <vector>
 
 #if defined(VARJOXR_ENABLE_D3D11)
@@ -35,18 +36,24 @@ public:
 
 #if defined(VARJOXR_ENABLE_D3D11)
     virtual std::shared_ptr<XRTexture> createTextureFromD3D11Resource(
-        ID3D11Texture2D* texture,
-        DXGI_FORMAT srvFormat) = 0;
+        ID3D11Texture2D*,
+        DXGI_FORMAT) {
+        throw std::runtime_error("The active VarjoXR backend does not support D3D11 native texture wrapping.");
+    }
     virtual std::shared_ptr<XRTexture> createTextureFromD3D11Srv(
-        ID3D11ShaderResourceView* srv,
-        uint32_t width,
-        uint32_t height) = 0;
+        ID3D11ShaderResourceView*,
+        uint32_t,
+        uint32_t) {
+        throw std::runtime_error("The active VarjoXR backend does not support D3D11 SRV texture wrapping.");
+    }
 #endif
 
 #if defined(VARJOXR_ENABLE_D3D12)
     virtual std::shared_ptr<XRTexture> createTextureFromD3D12Resource(
-        ID3D12Resource* resource,
-        DXGI_FORMAT srvFormat) = 0;
+        ID3D12Resource*,
+        DXGI_FORMAT) {
+        throw std::runtime_error("The active VarjoXR backend does not support D3D12 native texture wrapping.");
+    }
 #endif
 };
 
