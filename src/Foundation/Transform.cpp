68f72af9b6c8ed8c7bc00380f0ba6f4c4a6152a5
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-08 14:03:58 +0900

    rewrite v0.1: remove experimental glm gtx include from Transform

--- src/Foundation/Transform.cpp
+++ src/Foundation/Transform.cpp
@@ -1,13 +1,13 @@
 #include <VarjoXR/Foundation/Transform.hpp>
 
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtx/quaternion.hpp>
+#include <glm/gtc/quaternion.hpp>
 
 namespace VarjoXR {
 
 glm::mat4 Transform::matrix() const {
     const glm::mat4 t = glm::translate(glm::mat4(1.0f), position);
-    const glm::mat4 r = glm::toMat4(rotation);
+    const glm::mat4 r = glm::mat4_cast(rotation);
     const glm::mat4 s = glm::scale(glm::mat4(1.0f), scale);
     return t * r * s;
 }
