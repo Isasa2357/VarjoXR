2c320de38909eed4f80a0a28dd69ff25df545fa6
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-08 18:08:27 +0900

    rewrite v0.1: update D3D12Helper pin for valid PSO depth defaults

--- cmake/VarjoXRDependencies.cmake
+++ cmake/VarjoXRDependencies.cmake
@@ -13,7 +13,7 @@ set(VARJOXR_D3D11HELPER_GIT_REPOSITORY "https://github.com/Isasa2357/D3D11Helper
 set(VARJOXR_D3D11HELPER_GIT_TAG "f225641faa621c8c67b7df78f691dbdfed99e8cc" CACHE STRING "Pinned D3D11Helper git commit/tag")
 
 set(VARJOXR_D3D12HELPER_GIT_REPOSITORY "https://github.com/Isasa2357/D3D12Helper.git" CACHE STRING "D3D12Helper git repository")
-set(VARJOXR_D3D12HELPER_GIT_TAG "27db741021c0019a6e43aaccb26a67a02e8d2217" CACHE STRING "Pinned D3D12Helper git commit/tag")
+set(VARJOXR_D3D12HELPER_GIT_TAG "05e043523d811ea457e7413664132825c6d51d31" CACHE STRING "Pinned D3D12Helper git commit/tag")
 
 set(VARJOXR_GLM_GIT_REPOSITORY "https://github.com/g-truc/glm.git" CACHE STRING "glm git repository")
 set(VARJOXR_GLM_GIT_TAG "1.0.3" CACHE STRING "Pinned glm git tag")
