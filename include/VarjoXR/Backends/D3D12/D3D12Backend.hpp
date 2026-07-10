6fb632ee19c25f6b46a04ddda163141968b0b93e
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-11 08:36:58 +0900

    feat: add D3D12 frame info getter

--- include/VarjoXR/Backends/D3D12/D3D12Backend.hpp
+++ include/VarjoXR/Backends/D3D12/D3D12Backend.hpp
@@ -14,7 +14,6 @@
 #include <VarjoXR/Core/XRTexture.hpp>
 
 struct ID3D12Resource;
-
 typedef enum DXGI_FORMAT DXGI_FORMAT;
 
 namespace VarjoXR::Backends::D3D12 {
@@ -22,29 +21,21 @@ namespace VarjoXR::Backends::D3D12 {
 struct D3D12BackendDesc {
     int32_t swapchainTextureCount = 3;
     int64_t varjoTextureFormat = 0;
-
-    // Direct3D alpha blending used while drawing Plane pixels into the swapchain.
     bool enableAlphaBlend = true;
-
-    // Varjo compositor layer alpha blending. Keep this enabled for MR overlays;
-    // otherwise the transparent clear color can become an opaque black layer.
     bool enableLayerAlphaBlend = true;
-
     uint32_t cbvSrvUavDescriptorCount = 2048;
     uint32_t rtvDescriptorCount = 256;
-
-    // Number of CPU/GPU frame slots used for command allocators and upload buffers.
-    // Must be at least 2 for non-blocking D3D12 submission; values below 2 are clamped.
     uint32_t frameResourceCount = 3;
 };
 
 class D3D12Texture final : public XRTexture {
 public:
-    D3D12Texture(D3D12CoreLib::D3D12Resource resource,
-                 D3D12CoreLib::D3D12DescriptorHandle srv,
-                 uint32_t width,
-                 uint32_t height,
-                 TextureOwnership ownership);
+    D3D12Texture(
+        D3D12CoreLib::D3D12Resource resource,
+        D3D12CoreLib::D3D12DescriptorHandle srv,
+        uint32_t width,
+        uint32_t height,
+        TextureOwnership ownership);
 
     D3D12CoreLib::D3D12Resource& resource() noexcept { return resource_; }
     const D3D12CoreLib::D3D12Resource& resource() const noexcept { return resource_; }
@@ -57,14 +48,19 @@ private:
 
 class D3D12Backend final : public IRenderBackend {
 public:
-    D3D12Backend(std::shared_ptr<D3D12CoreLib::D3D12Core> core, D3D12BackendDesc desc = {});
+    D3D12Backend(
+        std::shared_ptr<D3D12CoreLib::D3D12Core> core,
+        D3D12BackendDesc desc = {});
     ~D3D12Backend() override;
 
     BackendType backendType() const noexcept override { return BackendType::D3D12; }
     void initialize(std::shared_ptr<::VarjoSession> session) override;
     void beginFrame() override;
-    void render(const std::vector<std::unique_ptr<XRPlane>>& planes, const FrameContext& frameContext) override;
+    void render(
+        const std::vector<std::unique_ptr<XRPlane>>& planes,
+        const FrameContext& frameContext) override;
     void endFrame() override;
+    VarjoFrameInfoSnapshot frameInfoSnapshot() const override;
 
     std::shared_ptr<D3D12Texture> createTextureFromRGBA(
         const uint8_t* rgba,
