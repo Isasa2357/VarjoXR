0aff9d925c21dd468ceb7541e0ac110d953cd04b
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-11 08:37:54 +0900

    refactor: make D3D11 backend own Varjo frame synchronization

---
+++ src/Backends/D3D11/D3D11BackendExternalFrameInfo.cpp
@@ -0,0 +1,29 @@
+#include <VarjoToolkit/Core/VarjoFrameInfo.hpp>
+
+class VarjoXRRenderFrameInfo final : public VarjoFrameInfo {
+public:
+    using VarjoFrameInfo::VarjoFrameInfo;
+
+    bool waitSyncFromRenderer()
+    {
+        if (!valid()) return false;
+        varjo_WaitSync(session(), get());
+        return true;
+    }
+};
+
+#define VarjoFrameInfo VarjoXRRenderFrameInfo
+#define waitSync waitSyncFromRenderer
+#include "D3D11Backend.cpp"
+#undef waitSync
+#undef VarjoFrameInfo
+
+namespace VarjoXR::Backends::D3D11 {
+
+VarjoFrameInfoSnapshot D3D11Backend::frameInfoSnapshot() const
+{
+    if (!impl_ || !impl_->frameInfo) return {};
+    return impl_->frameInfo->snapshot();
+}
+
+} // namespace VarjoXR::Backends::D3D11
