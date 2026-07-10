cf9596f04ded61c79fb24e22776c546b64ec77ea
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-11 08:37:10 +0900

    feat: add D3D11 frame info getter

--- include/VarjoXR/Backends/D3D11/D3D11Backend.hpp
+++ include/VarjoXR/Backends/D3D11/D3D11Backend.hpp
@@ -13,7 +13,6 @@
 
 struct ID3D11ShaderResourceView;
 struct ID3D11Texture2D;
-
 typedef enum DXGI_FORMAT DXGI_FORMAT;
 
 namespace VarjoXR::Backends::D3D11 {
@@ -21,26 +20,23 @@ namespace VarjoXR::Backends::D3D11 {
 struct D3D11BackendDesc {
     int32_t swapchainTextureCount = 3;
     int64_t varjoTextureFormat = 0;
-
-    // Direct3D alpha blending used while drawing Plane pixels into the swapchain.
     bool enableAlphaBlend = true;
-
-    // Varjo compositor layer alpha blending. Keep this enabled for MR overlays;
-    // otherwise the transparent clear color can become an opaque black layer.
     bool enableLayerAlphaBlend = true;
 };
 
 class D3D11Texture final : public XRTexture {
 public:
-    D3D11Texture(D3D11CoreLib::D3D11Resource resource,
-                 D3D11CoreLib::ComPtr<ID3D11ShaderResourceView> srv,
-                 uint32_t width,
-                 uint32_t height,
-                 TextureOwnership ownership);
+    D3D11Texture(
+        D3D11CoreLib::D3D11Resource resource,
+        D3D11CoreLib::ComPtr<ID3D11ShaderResourceView> srv,
+        uint32_t width,
+        uint32_t height,
+        TextureOwnership ownership);
 
-    D3D11Texture(D3D11CoreLib::ComPtr<ID3D11ShaderResourceView> srv,
-                 uint32_t width,
-                 uint32_t height);
+    D3D11Texture(
+        D3D11CoreLib::ComPtr<ID3D11ShaderResourceView> srv,
+        uint32_t width,
+        uint32_t height);
 
     D3D11CoreLib::D3D11Resource& resource() noexcept { return resource_; }
     const D3D11CoreLib::D3D11Resource& resource() const noexcept { return resource_; }
@@ -53,14 +49,19 @@ private:
 
 class D3D11Backend final : public IRenderBackend {
 public:
-    D3D11Backend(std::shared_ptr<D3D11CoreLib::D3D11Core> core, D3D11BackendDesc desc = {});
+    D3D11Backend(
+        std::shared_ptr<D3D11CoreLib::D3D11Core> core,
+        D3D11BackendDesc desc = {});
     ~D3D11Backend() override;
 
     BackendType backendType() const noexcept override { return BackendType::D3D11; }
     void initialize(std::shared_ptr<::VarjoSession> session) override;
     void beginFrame() override;
-    void render(const std::vector<std::unique_ptr<XRPlane>>& planes, const FrameContext& frameContext) override;
+    void render(
+        const std::vector<std::unique_ptr<XRPlane>>& planes,
+        const FrameContext& frameContext) override;
     void endFrame() override;
+    VarjoFrameInfoSnapshot frameInfoSnapshot() const override;
 
     std::shared_ptr<D3D11Texture> createTextureFromRGBA(
         const uint8_t* rgba,
